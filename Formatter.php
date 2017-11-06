45bdc06b3221cf6237660771de8ea8194d3eaf49
Hugo Ferreira <hugoofab@gmail.com>
2017-11-06 20:45:11 -0200

    formatter should be static

--- Formatter.php
+++ Formatter.php
@@ -7,7 +7,7 @@ class Formatter {
 	/**
 	* convert dd/mm/yyyy to yyyy-mm-dd
 	*/
-    public function dateToDb ( $date ) {
+    public static function dateToDb ( $date ) {
     	$newTime = strtotime ( str_replace('/', '-', $date ) ) ;
         return date ( 'Y-m-d', $newTime );
     }
@@ -15,7 +15,7 @@ class Formatter {
     /**
     * convert 1.000.000,10 to (float) 1000000.10
     */
-    public function brMoneyToFloat ( $money ) {
+    public static function brMoneyToFloat ( $money ) {
     	$money = preg_replace('/[^\d\,]/', '', $money ) ;
 		$money = (float) preg_replace('/,/','.',$money);
 		return $money ;
